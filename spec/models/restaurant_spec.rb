b867c7842f745ef408adf218d24bd516d7c7a380
Elin <elinnet@gmail.com>
2015-07-22 12:44:51 +0100

    add of validators on ratings - all tests pass

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -10,6 +10,9 @@ describe Restaurant, type: :model do
     expect(restaurant).not_to be_valid
   end
 
-
-
+  it "is not valid undless it has a unique name" do
+    Restaurant.create(name: "Moe's Tavern")
+    restaurant = Restaurant.new(name: "Moe's Tavern")
+    expect(restaurant).to have(1).error_on(:name)
+  end
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -2,7 +2,6 @@ require 'spec_helper'
 
 describe Review, type: :model do
 
-
   it { is_expected.to belong_to :restaurant }
 
   it 'will be deleted when restaurant deleted' do
@@ -10,5 +9,12 @@ describe Review, type: :model do
     kfc.reviews.create(thoughts: 'so so')
     kfc.destroy
     expect(Review.find_by thoughts: 'so so').to equal nil
+  end
+end
+
+describe Review, :type => :model do
+  it 'is invalid if the rating is more than 5' do
+    review = Review.new(rating:10)
+    expect(review).to have(1).error_on(:rating)
   end
 end
