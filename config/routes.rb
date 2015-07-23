4b4a03e005779ad5cca81f9943e57849a9b966b4
jonathansayer <jon.sayer1@gmail.com>
2015-07-23 10:47:48 +0100

    trying to understand devise_scope

--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,7 @@ Rails.application.routes.draw do
 
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
-  # devise_scope :user do
-  #   get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+  # devise_scope :user do 
+  #   get 'sign_out', :to => 'devise/sessions#destroy'
   # end
-
 end
